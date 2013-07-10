d46789034f3818a06a410e5f2471d76c2aef06e3
unknown <tso3805@COL-ISD-D62361.columbia.corp.shelterinsurance.com>
2013-07-10 16:49:49 -0500

    More files for your pleasure

--- mods/conundrum.blitzen-v1.0/blitzen.groovy
+++ mods/conundrum.blitzen-v1.0/blitzen.groovy
@@ -1,4 +1,12 @@
 def eb = vertx.eventBus
 def config = container.config
 
+eb.registerHandler("conundrum.blitzen") { message ->
+	println message.body.query
+	def result = [:]
+	result['result'] = message.body.query
+	message.reply(result)
+}
+
 println 'up, up and away..'
+
